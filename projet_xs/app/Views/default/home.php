5f8abc48aa6d187be81529024e283a97924ee919
spartdesign972 <spartdesign972@gmail.com>
2017-04-04 23:00:36 -0300

    debut design front

--- projet_xs/app/Views/default/home.php
+++ projet_xs/app/Views/default/home.php
@@ -1,7 +1,80 @@
 <?php $this->layout('layout', ['title' => 'Accueil']) ?>
 
 <?php $this->start('main_content') ?>
-	<h2>Let's code.</h2>
-	<p>Vous avez atteint la page d'accueil. Bravo.</p>
-	<p>Et maintenant, RTFM dans <strong><a href="../docs/tuto/" title="Documentation de W">docs/tuto</a></strong>.</p>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="<?= $this->assetUrl('img/heart.jpg') ?>" alt="Chania" >
+    </div>
+
+    <div class="item">
+      <img src="<?= $this->assetUrl('img/heart.jpg') ?>" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="<?= $this->assetUrl('img/heart.jpg') ?>" alt="Flower">
+    </div>
+
+    <div class="item">
+      <img src="<?= $this->assetUrl('img/heart.jpg') ?>" alt="Flower">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+	<section class="presentation">
+			<div class="colored">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-sm-8 text-center">
+							<h1>Bonjour, comment tu t'appel ?</h1>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione iure amet praesentium libero alias autem voluptas dolor facere. Ad, debitis, fugiat molestiae adipisci nemo odio! Rerum sapiente nostrum pariatur, velit!</p>
+						</div>
+					</div>
+				</div>
+			</div>
+	</section>
+
+  <section class="howto">
+    <div class="colored">
+      <div class="container">
+        <h1>Comment creer votre tshirt ?!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae velit sit, quae debitis ea ratione ex excepturi rem vero sequi enim minus inventore nulla et aut, laboriosam optio, officia. Iusto!</p> 
+      </div>
+    </div>
+  </section>
+
+  <section class="selection">
+    <h1>Sélection de la semaine</h1>
+
+    
+  </section>
+
+  <section class="infos">
+    <h1>Flash info !</h1>
+
+  </section>
 <?php $this->stop('main_content') ?>
+
+<?php $this->start('footer') ?>
+
+
+<?php $this->stop('footer') ?>
--- projet_xs/app/Views/layout.php
+++ projet_xs/app/Views/layout.php
@@ -4,20 +4,89 @@
 	<meta charset="UTF-8">
 	<title><?= $this->e($title) ?></title>
 
+	<link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/bootstrap.min.css') ?> ">
+
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+
+	<link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/font-awesome.min.css') ?>">
+
+	<link href="https://fonts.googleapis.com/css?family=Oleo+Script+Swash+Caps|Gochi+Hand|PT+Sans" rel="stylesheet">
+	
+	<?= $this->section('link') ?>
+
 </head>
 <body>
-	<div class="container">
 		<header>
-			<h1>W :: <?= $this->e($title) ?></h1>
+
+			<div class="page-header">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-sm-6">
+				  		<img src="./assets/img/logo.png" alt="logo">
+				  	</div>
+				  	<div class="col-xs-12 col-sm-6">
+				  		<div class="col-xs-6 text-right">
+				  			<a href="#" title="user"><i class="fa fa-user-circle-o fa-4x" 
+				  			aria-hidden="true"></i></a>
+				  		</div>
+				  		<div class="col-xs-6 text-left">
+				  			<a href="#" title="user"><i class="fa fa-shopping-cart fa-4x" aria-hidden="true"></i></a>
+				  		</div>
+				  	</div>
+				  </div>
+			  </div>
+			</div>
+
+			<nav class="navbar navbar-default" role="navigation" data-spy="affix" data-offset-top="150">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Title</a>
+					</div>
+			
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse navbar-ex1-collapse">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+						</ul>
+						<!-- <ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Link</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+									<li><a href="#">Something else here</a></li>
+									<li><a href="#">Separated link</a></li>
+								</ul>
+							</li>
+						</ul> -->
+					</div><!-- /.navbar-collapse -->
+				</div>
+			</nav>
+			<?= $this->section('header') ?>
 		</header>
 
-		<section>
-			<?= $this->section('main_content') ?>
-		</section>
+		<main class="container-fluid">
+				<?= $this->section('main_content') ?>
+		</main>
 
 		<footer>
+			<?= $this->section('footer') ?>
 		</footer>
-	</div>
+
+	<script type="text/javascript" src="<?= $this->assetUrl('js/jquery.min.js') ?>"></script>
+
+	<script type="text/javascript" src="<?= $this->assetUrl('js/bootstrap.min.js') ?>"></script>
+
+	<?= $this->section('script') ?>
+
 </body>
 </html>
