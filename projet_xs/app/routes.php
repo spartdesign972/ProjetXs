93d2317bc4a12f1b6327cf251ebb87024a1d2e4f
Morgan <morganpaulparvenu@gmail.com>
2017-04-11 14:31:32 -0400

    BACK - commit routes.php

--- projet_xs/app/routes.php
+++ projet_xs/app/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 $w_routes = array(
+
     ['GET', '/', 'Default#home', 'default_home'],
     // Route ou les utilisateurs sont rediriger pour se connecter
     ['GET|POST', '/connection', 'Default#connect', 'login'],
@@ -16,7 +17,13 @@ $w_routes = array(
 
     ['GET|POST', '/contactez-nous', 'Default#contact', 'default_contact'],
 
+    // Administration BACK
     ['GET|POST', '/admin', 'Admin#showadmin', 'admin_showadmin'],
+    ['GET|POST', '/admin/login', 'Admin#login', 'admin_login'],		
+    ['GET|POST', '/admin/logout', 'Admin#logout', 'admin_logout'],
+    ['GET|POST', '/admin/users', 'Admin#users', 'admin_users'],
+    ['GET|POST', '/admin/user_details/[i:id]', 'Admin#user_details', 'admin_user_details'],
+    ['GET|POST', '/admin/delete_user', 'Admin#delete_user', 'admin_delete_user'],
 
     ['GET|POST', '/admin/listDesigns', 'Admin#showDesigns', 'admin_showDesigns'],
     //Page de personnalisation des Tshirt
@@ -25,7 +32,6 @@ $w_routes = array(
     ['GET|POST', '/useradmin', 'Users#showuser', 'user_showuser'],
 
     ['GET|POST', '/listorders', 'Users#listOrders', 'users_listOrders'],
-    ['GET|POST', '/vieworders/[i:id]','Users#viewOrder', 'users_viewOrder'],
-    
+    ['GET|POST', '/vieworders/[i:id]','Users#viewOrder', 'users_viewOrder'],    
     
 );
