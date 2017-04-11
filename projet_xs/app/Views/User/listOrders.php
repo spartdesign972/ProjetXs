898dd3bec123cd3a263bb18d6382831627747d01
Steeve <spartdesign972@gmail.com>
2017-04-11 10:43:14 -0400

    correctifs

--- projet_xs/app/Views/User/listOrders.php
+++ projet_xs/app/Views/User/listOrders.php
@@ -1,61 +1,47 @@
 <?php $this->layout('layout', ['title' => 'Vos commandes']) ?>
 <?php $this->start('main_content'); ?>
-<div class="container">
 
-<h1></h1>
-<!-- En-Tête de Présentation -->
-
-<div class="contact col-xs-12">
-
-<h1>Les Commandes</h1>
-
-	<br>
-
-	<table class="table">
-
-		<thead>
-			<tr>
-				<th>Date de commande</th>
-				<th>Commande</th>
-				<th>Status</th>
-				<th>Factures</th>
-			</tr>
-		</thead>
-		<?php foreach($Order as $viewOrder): ?>
-		<tbody>
-
+	<h1></h1>
+	<!-- En-Tête de Présentation -->
+	<div class="contact col-xs-12">
+		<h1>Les Commandes</h1>
+		<br>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Date de commande</th>
+					<th>Commande</th>
+					<th>Status</th>
+					<th>Factures</th>
+				</tr>
+			</thead>
+			<?php foreach($Order as $viewOrder): ?>
+			<tbody>
 				<tr>
 					<td><?=$viewOrder['date_create'];?></td>
 					<td><?='Commande N°: ' . $viewOrder['id'];?></td>
 					<td><?=$viewOrder['status'];?></td>
-
 					<td>
-						<a href="<?=$this->url('#')?>"><button type="button" class="btn btn-info btn-sm"><span class="glyphicon glyphicon-eye-open"></span>
-							Visualiser cette commande</button>
-						</a>
-					</td>
-
-					<td>
-						<a href="admin/modif_recipe.php?id=<?=$viewOrder['id'];?>">
+						<a href="<?=$this->url('users_viewOrder')?>"><button type="button" class="btn btn-info btn-sm"><span class="glyphicon glyphicon-eye-open"></span>
+						Visualiser cette commande</button>
+					</a>
+				</td>
+				<td>
+					<a href="admin/modif_recipe.php?id=<?=$viewOrder['id'];?>">
 						<button type="button" class="btn btn-info btn-sm">
-							<span class="glyphicon glyphicon-edit"></span> Modifier
+						<span class="glyphicon glyphicon-edit"></span> Modifier
 						</button>
 					</a>
 				</td>
-
 				<td>
 					<a href="admin/delete_recipe.php?id=<?=$viewOrder['id'];?>"><button type="button" class="btn btn-info btn-sm">
 						<span class="glyphicon glyphicon-remove"></span>Remove</button>
-				</a>
-			</td>
-
-		</tr>
-</tbody>
-	<?php endforeach; ?>
-</table>
-
-</div>
-
+					</a>
+				</td>
+			</tr>
+		</tbody>
+		<?php endforeach; ?>
+	</table>
 </div>
 
 <?php $this->stop('main_content'); ?>
