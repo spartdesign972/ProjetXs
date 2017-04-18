8d3420403ae74cd23149dc988fe4b1f37a41d460
Steeve <spartdesign972@gmail.com>
2017-04-17 22:04:04 -0400

    correctif du commit

--- projet_xs/app/Views/default/home.php
+++ projet_xs/app/Views/default/home.php
@@ -78,71 +78,27 @@
 <!--
 *************************** section how to *************************
 -->
-
 <section class="howto text-center">
   <h1>Comment personnaliser soi-même un t-shirt ?</h1>
   <div class="container text-center wow bounceInUp">
-  <ul>
-    <div class="col-xs-6 col-sm-3 chevron">
-      <li><h2>1</h2><br>Ouvrez le T-shirt Designer et choisissez parmi plus de 170 produits.</li>
-    </div>
-
-    <div class="col-xs-6 col-sm-3 chevron">
-      <li><h2>2</h2><br>Trouvez un ou plusieurs designs ou bien importez vos designs, photos ou textes.</li>
+    <ul>
+      <div class="col-xs-6 col-sm-3 chevron">
+        <li><h2>1</h2><br>Ouvrez le T-shirt Designer et choisissez parmi plus de 170 produits.</li>
+      </div>
+      <div class="col-xs-6 col-sm-3 chevron">
+        <li><h2>2</h2><br>Trouvez un ou plusieurs designs ou bien importez vos designs, photos ou textes.</li>
       </div>
       <div class="col-xs-6 col-sm-3 chevron">
         <li><h2>3</h2><br>Couleur, position du design, de nombreuses options sont à votre disposition.</li>
-        </div>
-        <div class="col-xs-6 col-sm-3">
-          <li><h2>4</h2><br>Ajoutez le t-shirt au panier, validez et vous recevrez votre commande quelques jours après.</li>
-          </div>
-        </ul>
-        <a class="btn  btn-primary " href="#">Personnaliser un t-shirt</a>
-        </div>
-      </section>
-     
-      <!-- <section class="howto">
-        <div class="container text-center wow bounceInUp">
-          <h1>Comment créer votre tshirt ?</h1>
-          <p>Du mal a créer votre T-shirt idéal ? Ne paniquez pas et suivez juste ces quelques petites étapes !</p>
-          <div class="row">
-            <div class="col-xs-6 col-sm-3"><h3>etape 1</h3></div>
-            <div class="col-xs-6 col-sm-3"><h3>etape 2</h3></div>
-            <div class="col-xs-6 col-sm-3"><h3>etape 3</h3></div>
-            <div class="col-xs-6 col-sm-3"><h3>etape 4</h3></div>
-          </div>
-        </div>
-      </section> -->
-      <!--*************************** fin how to *******************************-->
-      <!--
-      *************************** section selection *************************
-      -->
-      <section class="selection">
-        <div class="container text-center">
-          <div class="colored">
-            <h1>Sélection de la semaine</h1>
-            <div class="row">
-              
-              <?php foreach($productsSelection as $product) : ?>
-              <div class="col-sm-12 col-md-4 wow fadeInUp" data-wow-offset="200">
-                <div class="thumbnail">
-                  <img src="<?= $this->assetUrl('upload/' .$product['model']) ?>" alt="">
-                  <div class="caption">
-                    <h3 class="text-center"><?= $product['design_label'] ?></h3>
-                    <p class="design-author-name">Créer par : <a href="<?= $this->url('default_membredesignmembre', ['id'=>$product['user_id']]); ?>" title=""><?= $product['username'] ?></a></p>
-                    <!-- <p><a href="#" class="btn btn-default" role="button">Voir le produit</a></p> -->
-                    <div class="row">
-                      <p class="col-xs-6 text-left nb-like"><?= $product['likes_count'] ?> <i class="fa fa-heart" aria-hidden="true"></i></p>
-                      <?php if(!empty($w_user)) : ?>
-                      <p class="col-xs-6 text-right my-like" data-user="<?= $w_user['id'] ?>" data-id="<?=$product['id'] ?>"></p>
-                      <p class="col-xs-12 text-center"><a href="<?=$this->url('cart_createcart', ['id' => $product['id']]) ?>" class="btn btn-default addCart" role="button">Ajouter au panier</a></p>
-                      <?php endif; ?>
-                    </div>
-                  </div>
-                </div>
-
+      </div>
+      <div class="col-xs-6 col-sm-3">
+        <li><h2>4</h2><br>Ajoutez le t-shirt au panier, validez et vous recevrez votre commande quelques jours après.</li>
+      </div>
+    </ul>
+    <a class="btn  btn-primary " href="#">Personnaliser un t-shirt</a>
   </div>
 </section>
+
 <!--*************************** fin how to *******************************-->
 <!--
 *************************** section selection *************************
@@ -151,37 +107,36 @@
   <div class="container text-center">
     <div class="colored">
       <h1>Sélection de la semaine</h1>
-      <div class="row">
-      
+      <div class="row"> 
         <?php foreach($productsSelection as $product) : ?>
-
         <div class="col-sm-12 col-md-4 wow fadeInUp" data-wow-offset="200">
-          <div class="thumbnail greyfilter">
+          <div class="thumbnail">
             <img src="<?= $this->assetUrl('upload/' .$product['model']) ?>" alt="">
             <div class="caption">
-
               <h3 class="text-center"><?= $product['design_label'] ?></h3>
               <p class="design-author-name">Créer par : <a href="<?= $this->url('default_membredesignmembre', ['id'=>$product['user_id']]); ?>" title=""><?= $product['username'] ?></a></p>
               <!-- <p><a href="#" class="btn btn-default" role="button">Voir le produit</a></p> -->
-
               <div class="row">
                 <p class="col-xs-6 text-left nb-like"><?= $product['likes_count'] ?> <i class="fa fa-heart" aria-hidden="true"></i></p>
                 <?php if(!empty($w_user)) : ?>
-                  <p class="col-xs-6 text-right my-like" data-user="<?= $w_user['id'] ?>" data-id="<?=$product['id'] ?>"></p>
-                  <p class="col-xs-12 text-center"><a href="<?=$this->url('cart_createcart', ['id' => $product['id']]) ?>" class="btn btn-default addCart" role="button">Ajouter au panier</a></p>
+                <p class="col-xs-6 text-right my-like" data-user="<?= $w_user['id'] ?>" data-id="<?=$product['id'] ?>"></p>
+                <p class="col-xs-12 text-center"><a href="<?=$this->url('cart_createcart', ['id' => $product['id']]) ?>" class="btn btn-default addCart" role="button">Ajouter au panier</a></p>
                 <?php endif; ?>
-
-              </div>
-              <?php endforeach; ?>
             </div>
           </div>
+          </div>
+          </div>
+          <?php endforeach; ?>
         </div>
       </div>
-    </div>
-  </section>
-  <!--*************************** fin selection *******************************-->
-  <div class="clearfix"></div>
-  <?php $this->stop('main_content') ?>
-  <?php $this->start('footer') ?>
-  <?php $this->insert('inc/_footer') ?>
-  <?php $this->stop('footer') ?>
+  </div>
+</section>
+      <!--*************************** fin how to *******************************-->
+      <!--
+   
+        <div class="clearfix"></div>
+        <?php $this->stop('main_content') ?>
+
+        <?php $this->start('footer') ?>
+          <?php $this->insert('inc/_footer') ?>
+        <?php $this->stop('footer') ?>
